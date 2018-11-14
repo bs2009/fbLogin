1b3fb098eeba1cd52097f7443634ec173cd4ed3e
Bill Song <bsong2009@gmail.com>
2018-11-13 20:56:33 -0600

    Initial Commit

---
+++ fbLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fbLogin
+//
+//  Created by William Song on 11/13/18.
+//  Copyright © 2018 William Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
